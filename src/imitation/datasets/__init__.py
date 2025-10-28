f580bfa4e2bfd42a21ebf59aa25f8094f8cc343b
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-10-27 23:13:59 -0400

    Add OGBench Dataset

--- src/imitation/datasets/__init__.py
+++ src/imitation/datasets/__init__.py
@@ -1,4 +1,11 @@
 from .minari_dataset import MinariDataset
+from .ogbench_dataset import OGBenchDataset
+
+# MINARI
 
 PenHumanV2 = MinariDataset.from_name("D4RL/pen/human-v2")
 BabyAIPickupOptimalFull = MinariDataset.from_name("minigrid/BabyAI-Pickup/optimal-fullobs-v0")
+
+# OGBENCH
+
+AntMazeMediumNavigateV0 = OGBenchDataset.from_name('antmaze-medium-navigate-v0')
