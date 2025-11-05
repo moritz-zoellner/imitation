1534e6d9e37be48882ab91036e0a18ce5c6111b8
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-11-05 16:06:28 -0500

    Implement JAX compatible dataset

--- src/imitation/algorithms/offline_rl/utils/datasets.py
+++ src/imitation/algorithms/offline_rl/utils/datasets.py
@@ -51,35 +51,40 @@ class MyDataset(FrozenDict):
             **fields: Keys and values of the dataset.
         """
         data = fields
+        if "observations" not in data and "observation" in data:
+            data["observations"] = data["observation"]
+            del data["observation"]
+        if "next_observations" not in data and "next_observation" in data:
+            data["next_observations"] = data["next_observation"]
+            del data["next_observation"]
         assert 'observations' in data
-        if freeze:
-            jax.tree_util.tree_map(lambda arr: arr.setflags(write=False), data)
+        data = jax.tree_util.tree_map(lambda arr: jnp.array(arr), data)
         return cls(data)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.size = get_size(self._dict)
-        if 'valids' in self._dict:
-            (self.valid_idxs,) = np.nonzero(self['valids'] > 0)
+        self.size = jnp.array(self._dict["observations"].shape[:-1])
+        # if 'valids' in self._dict:
+        #     (self.valid_idxs,) = np.nonzero(self['valids'] > 0)
 
-    def get_random_idxs(self, num_idxs):
+    def get_random_idxs(self, key, num_idxs):
         """Return `num_idxs` random indices."""
-        if 'valids' in self._dict:
-            return self.valid_idxs[np.random.randint(len(self.valid_idxs), size=num_idxs)]
-        else:
-            return np.random.randint(self.size, size=num_idxs)
+        def f(key, upper_bound):
+            return jax.random.randint(key, (num_idxs,), 0, upper_bound)
 
-    def sample(self, batch_size, idxs=None):
+        keys = jax.random.split(key, self.size.shape[0])
+        idxs = jax.vmap(f, in_axes=(0, 0))(keys, self.size)
+        return idxs
+
+    def sample(self, key, batch_size, idxs=None):
         """Sample a batch of transitions."""
         if idxs is None:
-            idxs = self.get_random_idxs(batch_size)
+            idxs = self.get_random_idxs(key, batch_size)
         return self.get_subset(idxs)
 
     def get_subset(self, idxs):
         """Return a subset of the dataset given the indices."""
-        result = jax.tree_util.tree_map(lambda arr: arr[idxs], self._dict)
-        if 'next_observations' not in result:
-            result['next_observations'] = self._dict['observations'][np.minimum(idxs + 1, self.size - 1)]
+        result = jax.tree_util.tree_map(lambda arr: arr[(*idxs, ...)], self._dict)
         return result
 
 
