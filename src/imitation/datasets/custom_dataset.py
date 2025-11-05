35ce0cc319c3ca86230814253041b9f16f8431a4
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-11-05 16:06:51 -0500

    Use MyDataset to implement CustomDataset

--- src/imitation/datasets/custom_dataset.py
+++ src/imitation/datasets/custom_dataset.py
@@ -3,8 +3,11 @@ from dataclasses import dataclass
 from importlib.resources import files
 
 import numpy as np
+import jax.numpy as jnp
 
 import imitation
+import imitation.environments
+from imitation.algorithms.offline_rl.utils.datasets import MyDataset
 
 from .base import BaseDataset
 
@@ -12,12 +15,22 @@ from .base import BaseDataset
 @dataclass(kw_only=True)
 class CustomDataset(BaseDataset):
 
-    data: Any
+    dataset_impl: Any
+    env: Any
 
-    def get_eval_env(self):
-        raise NotImplementedError()
+    def get_eval_env(self, episode_length: int, action_repeat: int):
+        return self.env.wrap_for_eval(episode_length, action_repeat)
+
+    def sample_episodes(self, batch_size):
+        return self.dataset_impl.sample(batch_size)
+
+    def sample(self, key, batch_size):
+        return self.dataset_impl.sample(key, batch_size)
 
     @staticmethod
-    def from_resource_path(path):
+    def from_resource_path(path, env_name):
         full_path = files(imitation) / path
-        return CustomDataset(data=np.load(full_path))
+        data = jnp.load(full_path)
+        dataset = MyDataset.create(**data)
+        env = getattr(imitation.environments, env_name)
+        return CustomDataset(dataset_impl=dataset, env=env)
